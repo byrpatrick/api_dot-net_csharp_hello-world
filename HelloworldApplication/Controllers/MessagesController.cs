46f3980d15b47ef169961f1de7614bba28b1fc40
Andrea Chiarelli <andrea.chiarelli@auth0.com>
2021-05-12 11:55:52 +0200

    Add CORS support

--- HelloworldApplication/Controllers/MessagesController.cs
+++ HelloworldApplication/Controllers/MessagesController.cs
@@ -2,30 +2,30 @@
 
 namespace HelloworldApplication.Controllers
 {
-    [ApiController]
-    [Route("api/[controller]")]
-    public class MessagesController : ControllerBase
+  [ApiController]
+  [Route("api/[controller]")]
+  public class MessagesController : ControllerBase
+  {
+    private const string publicMessage = "The API doesn't require an access token to share this message.";
+    private const string protectedMessage = "The API successfully validated your access token.";
+    private const string adminMessage = "The API successfully recognized you as an admin.";
+
+    [HttpGet("public")]
+    public string GetPublicMessage()
     {
-        private const string publicMessage = "The API doesn't require an access token to share this message.";
-        private const string protectedMessage = "The API successfully validated your access token.";
-        private const string adminMessage = "The API successfully recognized you as an admin.";
-
-        [HttpGet("public")]
-        public string GetPublicMessage()
-        {
-            return publicMessage;
-        }
-
-        [HttpGet("protected")]
-        public string GetProtectedMessage()
-        {
-            return protectedMessage;
-        }
-
-        [HttpGet("admin")]
-        public string GetAdminMessage()
-        {
-            return adminMessage;
-        }
+      return publicMessage;
     }
+
+    [HttpGet("protected")]
+    public string GetProtectedMessage()
+    {
+      return protectedMessage;
+    }
+
+    [HttpGet("admin")]
+    public string GetAdminMessage()
+    {
+      return adminMessage;
+    }
+  }
 }
--- HelloworldApplication/Startup.cs
+++ HelloworldApplication/Startup.cs
@@ -6,38 +6,45 @@ using Microsoft.Extensions.Hosting;
 
 namespace HelloworldApplication
 {
-    public class Startup
+  public class Startup
+  {
+    public Startup(IConfiguration configuration)
     {
-        public Startup(IConfiguration configuration)
-        {
-            Configuration = configuration;
-        }
-
-        public IConfiguration Configuration { get; }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-
-            services.AddControllers();
-        }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
-        {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-            }
-
-            app.UseHttpsRedirection();
-
-            app.UseRouting();
-
-            app.UseEndpoints(endpoints =>
-            {
-                endpoints.MapControllers();
-            });
-        }
+      Configuration = configuration;
     }
+
+    public IConfiguration Configuration { get; }
+
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+      services.AddCors(options =>
+      {
+        options.AddDefaultPolicy(
+              builder =>
+              {
+                builder.AllowAnyOrigin();
+              });
+      });
+      services.AddControllers();
+    }
+
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+    {
+      if (env.IsDevelopment())
+      {
+        app.UseDeveloperExceptionPage();
+      }
+
+      app.UseHttpsRedirection();
+      app.UseRouting();
+      app.UseCors();
+
+      app.UseEndpoints(endpoints =>
+      {
+        endpoints.MapControllers();
+      });
+    }
+  }
 }
